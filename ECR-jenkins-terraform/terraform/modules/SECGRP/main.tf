64b83c467df0e5da96dead466bded5e8fdbdfe87
Solomon Onwuasoanya <onwuasoanyasc@gmail.com>
2023-09-03 10:17:39 +0100

    Update main.tf

--- ECR-jenkins-terraform/terraform/modules/SECGRP/main.tf
+++ ECR-jenkins-terraform/terraform/modules/SECGRP/main.tf
@@ -1,38 +1,5 @@
 # # create jenkins security group
 
-
-# resource "aws_security_group" "jenkins-http-ssh-sg" {
-#   vpc_id      = var.vpc_id
-#   name        = "jenkins-sg"
-#   description = "sec group for jenkins http access"
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = merge(
-#     var.tags,
-#     {
-#       Name = format("%s-jenkins-sg", var.name)
-#     },
-#   )
-# }
-
 resource "aws_security_group" "jenkins-http-ssh-sg" {
   name        = "jenkins-sg"
   description = "jenkins Security Group"
