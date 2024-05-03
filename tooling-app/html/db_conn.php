dbe7cbda762bb171161870c6bdcb2e35fac34e2f
solomon onwuasoanya <onwuasoanyasc@gmail.com>
2024-05-03 19:06:15 +0100

    updated db_conn.php and Dockerfile

--- tooling-app/html/db_conn.php
+++ tooling-app/html/db_conn.php
@@ -17,7 +17,7 @@ $dbname = $_ENV["MYSQL_DBNAME"]; // input dbname
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
